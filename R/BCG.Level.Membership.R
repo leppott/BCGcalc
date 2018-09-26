e287e1a8c67e3d9c25a962eafe0836bb480a9ea8
leppott <Erik.Leppo@tetratech.com>
2018-09-26 11:17:50 -0400

    v1.2.2.9015
    Update Rules.xlsx and Index_Name.  Issue #26

--- R/BCG.Level.Membership.R
+++ R/BCG.Level.Membership.R
@@ -29,7 +29,7 @@
 #' 
 #' # Import Rules
 #' df.rules <- read_excel(system.file("./extdata/Rules.xlsx"
-#'                              , package="BCGcalc"), sheet="BCG_PacNW_2018") 
+#'                              , package="BCGcalc"), sheet="BCG_PacNW_v1_500ct") 
 #' 
 #' # Calculate Metric Memberships
 #' df.Metric.Membership <- BCG.Metric.Membership(df.metric.values.bugs, df.rules)
@@ -56,7 +56,7 @@
 # 
 # # Import Rules
 # df.rules <- read_excel(system.file("./extdata/Rules.xlsx"
-#                              , package="BCGcalc"), sheet="BCG_PacNW_2018")
+#                              , package="BCGcalc"), sheet="BCG_PacNW_v1_500ct")
 # # Calculate Membership
 # df.metric.membership <- BCG.Metric.Membership(df.metric.values.bugs, df.rules)
 # #
--- R/BCG.Metric.Membership.R
+++ R/BCG.Metric.Membership.R
@@ -22,7 +22,7 @@
 #' 
 #' # Import Rules
 #' df.rules <- read_excel(system.file("./extdata/Rules.xlsx"
-#'                              , package="BCGcalc"), sheet="BCG_PacNW_2018") 
+#'                              , package="BCGcalc"), sheet="BCG_PacNW_v1_500ct") 
 #' 
 #' # Run function
 #' df.Metric.Membership <- BCG.Metric.Membership(df.metric.values.bugs, df.rules)
