577d4cb258e3c85662a8c50d5b8bda346b997720
leppott <Erik.Leppo@tetratech.com>
2018-11-16 15:58:43 -0500

    v1.3.2.9011
    Remove rarify function.  Issue #40

--- R/data.R
+++ R/data.R
@@ -1,18 +1,4 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' @title rarify example data
-#' 
-#' @description A dataset with example benthic macroinvertebrate data (600 count) 
-#' to be used with the rarify function.  Includes 12 samples.
-#' 
-#' @format A data frame with 223 rows and 28 variables:
-#' \describe{
-#'    \item{SampleID}{Sample ID}
-#'    \item{TaxaID}{unique taxonomic identifier}
-#'    \item{N_Taxa}{number of individuals in sample}
-#' }
-#' @source example data
-"data_bio2rarify"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @title Benthic Master Taxa List, BCG Pacific Northwest
 #' 
 #' @description Master taxa list for BCG model for Pacific Northwest; 
